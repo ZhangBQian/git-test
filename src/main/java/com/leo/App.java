87cc15274c9edde84c2bb83150054c1995590a07
ZhangBQian <443603264@qq.com>
2021-07-11 22:50:44 +0800

    第三次提交

--- src/main/java/com/leo/App.java
+++ src/main/java/com/leo/App.java
@@ -11,8 +11,8 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello Git" );
-        System.out.println("hello second1");
-        System.out.println("dier ");
+        System.out.println("新分支顶顶顶顶");
+        System.out.println("合并开始");
+        System.out.println("teet");
     }
 }
